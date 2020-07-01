fea2d72613a6cdd791b21a09c766de4489e7fff5
lmhc11 <lhcsupport5@lemonhc.com>
2020-07-01 10:23:38 +0900

    website1 프로젝트 생성

---
+++ src/test/java/com/bdh/website1/Website1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bdh.website1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Website1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
